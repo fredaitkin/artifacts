5980186d885028e43c83ac2a7acd7c0471cd471b
Melissa Aitkin <melissaaitkin@gmail.com>
2019-05-27 16:29:05 -0400

    add players migration

---
+++ database/migrations/2019_05_20_201849_create_players_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('team');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country')->nullable()
+            $table->date('birthdate')->nullable();
+            $table->integer('draft_year');
+            $table->integer('draft_round');
+            $table->integer('draft_position');
+            $table->timestamps();       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
