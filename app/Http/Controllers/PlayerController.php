014da001ced623adf3002e8636e6d2992fc2ac16
Melissa Aitkin <melissaaitkin@gmail.com>
2019-07-09 23:25:32 -0400

    updated database

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -25,7 +25,7 @@ class PlayerController extends Controller
      */
     public function index()
     {
-        $players = Player::sortable()->paginate(10);
+        $players = Player::sortable()->paginate(500);
         return view('players', ['players' => $players]);
     }
 
