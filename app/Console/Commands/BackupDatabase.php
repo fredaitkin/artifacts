c4998bde327cfd5831318a56579a577ce72f3919
Melissa Aitkin <melissaaitkin@gmail.com>
2019-10-19 16:12:07 -0400

    add artisan command to backup database

---
+++ app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Artifacts\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class BackupDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Backup the database';
+
+    /**
+     * The PHP process object
+     *
+     * @var Symfony\Component\Process\Process
+     */
+    protected $process;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->process = new Process(sprintf(
+            config('database.connections.mysql.dump.dump_binary_path') . 'mysqldump -u%s -p%s %s > %s',
+            config('database.connections.mysql.username'),
+            config('database.connections.mysql.password'),
+            config('database.connections.mysql.database'),
+            storage_path('backups/artifacts.sql')
+        ));
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->process->mustRun();
+            $this->info('The backup has been proceed successfully.');
+        } catch (ProcessFailedException $exception) {
+            $this->error('The backup process has been failed.');
+        }
+    }
+}
