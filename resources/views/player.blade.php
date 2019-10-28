9e46fe958567e5abe03580d611ba25253c05313c
Melissa Aitkin <melissaaitkin@gmail.com>
2019-10-27 21:21:23 -0400

    first column of two column form uneditable

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -23,110 +23,107 @@
                 </div>
             </div>
              @endif
- <div class="col-lg-6 float-left">
-                <!-- player Name -->
-                <div class="form-group">
-                    <label for="player" class="col-sm-3 control-label">First Name</label>
 
-                    <div class="col-sm-3">
-                        <input type="text" name="first_name" id="player->first_name" class="form-control" @if( ! empty($player->first_name)) value="{{$player->first_name}}" @endif>
-                    </div>
+            <div class="form-group">
+                <label for="player" class="col-sm-3 control-label">First Name</label>
 
+                <div class="col-sm-3">
+                    <input type="text" name="first_name" id="player->first_name" class="form-control" @if( ! empty($player->first_name)) value="{{$player->first_name}}" @endif>
                 </div>
 
-                <div class="form-group">
-                    <label for="last_name" class="col-sm-3 control-label">Last Name</label>
+            </div>
 
-                    <div class="col-sm-3">
-                        <input type="text" name="last_name" id="player->last_name" class="form-control" @if( ! empty($player->last_name)) value="{{$player->last_name}}" @endif>
-                    </div>
-                </div>
+            <div class="form-group">
+                <label for="last_name" class="col-sm-3 control-label">Last Name</label>
 
-                <div class="form-group">
-                    <label for="team" class="col-sm-3 control-label">Team</label>
-
-                    <div class="col-sm-3">
-                        <input type="text" name="team" id="player->team" class="form-control" @if( ! empty($player->team)) value="{{$player->team}}" @endif>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="city" class="col-sm-3 control-label">City</label>
-
-                    <div class="col-sm-4">
-                        <input type="text" name="city" id="player->city" class="form-control" @if( ! empty($player->city)) value="{{$player->city}}" @endif>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="state" class="col-sm-3 control-label">State</label>
-
-                    <div class="col-sm-4">
-                        <input type="text" name="state" id="player->state" class="form-control" @if( ! empty($player->state)) value="{{$player->state}}" @endif>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="country" class="col-sm-3 control-label">Country</label>
-                    <div class="col-sm-4">
-                        <input type="text" name="country" id="player->country" class="form-control" @if( ! empty($player->country)) value="{{$player->country}}" @endif>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="birthdate" class="col-sm-3 control-label">Birth Date</label>
-
-                    <div class="col-sm-3">
-                        <input type="text" name="birthdate" id="player->birthdate" class="form-control" @if( ! empty($player->birthdate)) value="{{$player->birthdate}}" @endif>
-                    </div>
+                <div class="col-sm-3">
+                    <input type="text" name="last_name" id="player->last_name" class="form-control" @if( ! empty($player->last_name)) value="{{$player->last_name}}" @endif>
                 </div>
             </div>
-            <div class="col-lg-6 float-right">
-                <div class="form-group">
-                    <label for="draft_year" class="col-sm-3 control-label">Draft Year</label>
 
-                    <div class="col-sm-3">
-                        <input type="text" name="draft_year" id="player->draft_year" class="form-control" @if( ! empty($player->draft_year)) value="{{$player->draft_year}}" @endif>
-                    </div>
+            <div class="form-group">
+                <label for="team" class="col-sm-3 control-label">Team</label>
+
+                <div class="col-sm-3">
+                    <input type="text" name="team" id="player->team" class="form-control" @if( ! empty($player->team)) value="{{$player->team}}" @endif>
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="draft_round" class="col-sm-3 control-label">Draft Round</label>
+            <div class="form-group">
+                <label for="city" class="col-sm-3 control-label">City</label>
 
-                    <div class="col-sm-3">
-                        <input type="text" name="draft_round" id="player->draft_round" class="form-control" @if( ! empty($player->draft_round)) value="{{$player->draft_round}}" @endif>
-                    </div>
+                <div class="col-sm-4">
+                    <input type="text" name="city" id="player->city" class="form-control" @if( ! empty($player->city)) value="{{$player->city}}" @endif>
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="draft_position" class="col-sm-3 control-label">Draft Position</label>
+            <div class="form-group">
+                <label for="state" class="col-sm-3 control-label">State</label>
 
-                    <div class="col-sm-3">
-                        <input type="text" name="draft_position" id="player->draft_position" class="form-control" @if( ! empty($player->draft_position)) value="{{$player->draft_position}}" @endif>
-                    </div>
+                <div class="col-sm-4">
+                    <input type="text" name="state" id="player->state" class="form-control" @if( ! empty($player->state)) value="{{$player->state}}" @endif>
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="debut_year" class="col-sm-3 control-label">Debut Year</label>
-
-                    <div class="col-sm-3">
-                        <input type="text" name="debut_year" id="player->debut_year" class="form-control" @if( ! empty($player->debut_year)) value="{{$player->debut_year}}" @endif>
-                    </div>
+            <div class="form-group">
+                <label for="country" class="col-sm-3 control-label">Country</label>
+                <div class="col-sm-4">
+                    <input type="text" name="country" id="player->country" class="form-control" @if( ! empty($player->country)) value="{{$player->country}}" @endif>
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="photo" class="col-sm-3 control-label">Photo</label>
+            <div class="form-group">
+                <label for="birthdate" class="col-sm-3 control-label">Birth Date</label>
 
-                    <div class="col-sm-5">
-                        <input type="file" name="photo" id="player->photo" class="form-control">
-                    </div>
+                <div class="col-sm-3">
+                    <input type="text" name="birthdate" id="player->birthdate" class="form-control" @if( ! empty($player->birthdate)) value="{{$player->birthdate}}" @endif>
                 </div>
-                <div class="form-group">
-                    <label for="previous_teams" class="col-sm-3 control-label">Previous Teams</label>
+            </div>
+     
+            <div class="form-group">
+                <label for="draft_year" class="col-sm-3 control-label">Draft Year</label>
 
-                    <div class="col-sm-8">
-                        <input type="text" name="previous_teams" id="player->previous_teams" class="form-control" @if( ! empty($player->previous_teams)) value="{{$player->previous_teams}}" @endif>
-                    </div>
+                <div class="col-sm-3">
+                    <input type="text" name="draft_year" id="player->draft_year" class="form-control" @if( ! empty($player->draft_year)) value="{{$player->draft_year}}" @endif>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="draft_round" class="col-sm-3 control-label">Draft Round</label>
+
+                <div class="col-sm-3">
+                    <input type="text" name="draft_round" id="player->draft_round" class="form-control" @if( ! empty($player->draft_round)) value="{{$player->draft_round}}" @endif>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="draft_position" class="col-sm-3 control-label">Draft Position</label>
+
+                <div class="col-sm-3">
+                    <input type="text" name="draft_position" id="player->draft_position" class="form-control" @if( ! empty($player->draft_position)) value="{{$player->draft_position}}" @endif>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="debut_year" class="col-sm-3 control-label">Debut Year</label>
+
+                <div class="col-sm-3">
+                    <input type="text" name="debut_year" id="player->debut_year" class="form-control" @if( ! empty($player->debut_year)) value="{{$player->debut_year}}" @endif>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="photo" class="col-sm-3 control-label">Photo</label>
+
+                <div class="col-sm-5">
+                    <input type="file" name="photo" id="player->photo" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="previous_teams" class="col-sm-3 control-label">Previous Teams</label>
+
+                <div class="col-sm-8">
+                    <input type="text" name="previous_teams" id="player->previous_teams" class="form-control" @if( ! empty($player->previous_teams)) value="{{$player->previous_teams}}" @endif>
                 </div>
             </div>
 
