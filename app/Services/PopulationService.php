5d3a8ff76b4ad5edb9f21d8a48b35ae7d5f8f23b
Melissa Aitkin <melissaaitkin@gmail.com>
2020-08-06 11:02:14 -0400

    add caching for population api calls

--- app/Services/PopulationService.php
+++ app/Services/PopulationService.php
@@ -3,6 +3,7 @@
 namespace Artifacts\Services;
 
 use Artifacts\Interfaces\PopulationServiceInterface;
+use Cache;
 
 class PopulationService implements PopulationServiceInterface {
 
@@ -12,22 +13,26 @@ class PopulationService implements PopulationServiceInterface {
      */
     public function getUSStatistics()
     {
-        // Get population of country
-        $url = 'https://api.census.gov/data/2019/pep/natmonthly?get=POP,MONTHLY_DESC&for=us:*';
-        $country = json_decode(file_get_contents($url));//var_dump($us);exit;
-        // Get population of each state
-        $url = 'https://api.census.gov/data/2019/pep/charagegroups?get=POP,NAME&for=state:*&DATE_CODE=12';
-        // TODO update Laravel version
-        //$response = Http::get($url);
-        $states = json_decode(file_get_contents($url));
-        $population = [];
-        // First record is header
-        unset($states[0]);
-        foreach($states as $state):
-            $population[$state[1]] = $state[0];
-        endforeach;
-        ksort($population);
-        return ['country_population' => $country[1][0], 'state_populations' => $population];
+        if (!Cache::has('us_population_statistics')):
+            // Get population of country
+            $url = 'https://api.census.gov/data/2019/pep/natmonthly?get=POP,MONTHLY_DESC&for=us:*';
+            $country = json_decode(file_get_contents($url));
+            // Get population of each state
+            $url = 'https://api.census.gov/data/2019/pep/charagegroups?get=POP,NAME&for=state:*&DATE_CODE=12';
+            // TODO update Laravel version
+            //$response = Http::get($url);
+            $states = json_decode(file_get_contents($url));
+            $population = [];
+            // First record is header
+            unset($states[0]);
+            foreach($states as $state):
+                $population[$state[1]] = $state[0];
+            endforeach;
+            ksort($population);
+            Cache::put('us_population_statistics', ['country_population' => $country[1][0], 'state_populations' => $population], 604800);
+        endif;
+
+        return Cache::get('us_population_statistics');
     }
 
 }
